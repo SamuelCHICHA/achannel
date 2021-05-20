5edbbb29f8ca1204e5275af685a6fcc039a53131
SamuelCHICHA <samuel.chicha.live.fr>
2021-05-20 11:56:37 +0200

    build: better error handling

--- ErrorHandler.py
+++ ErrorHandler.py
@@ -10,12 +10,26 @@ class ErrorHandler(commands.Cog):
     async def on_command_error(self, ctx, error):
         if isinstance(error, commands.CommandNotFound):
             return
-        if isinstance(error, commands.MissingRequiredArgument):
+        elif isinstance(error, commands.MissingRequiredArgument):
             await ctx.reply("Il manque des paramètres.")
             await ctx.send_help()
-        if isinstance(error, commands.TooManyArguments):
+        elif isinstance(error, commands.TooManyArguments):
             await ctx.reply("Trop de paramètres.")
-        if isinstance(error, commands.BotMissingPermissions):
+        elif isinstance(error, commands.BotMissingPermissions):
             logging.error(f"Bot doesn't have the right: {error.missing_perms} [{ctx.guild.name} ({ctx.guild.id})]")
-        if isinstance(error, commands.MissingPermissions):
+        elif isinstance(error, commands.MissingPermissions):
             await ctx.reply(f"Vous n'avez pas la permission de faire ça. {error.missing_perms}")
+        else:
+            original_exception = error.original
+            if isinstance(original_exception, commands.CommandError):
+                logging.warning(f"Discord error: {original_exception}")
+            else:
+                # Not a discord error
+                if isinstance(original_exception, KeyboardInterrupt):
+                    logging.critical("Shutting down.")
+                elif isinstance(original_exception, NameError):
+                    logging.error(original_exception)
+                elif isinstance(original_exception, AttributeError):
+                    logging.error(original_exception)
+                else:
+                    logging.warning(original_exception)
